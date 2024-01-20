827a2511bb01e18580399e4d2559c9a61680a143
hudaifa <hudaifa890@gmail.com>
2024-01-20 04:21:29 +0300

    check_sites

--- sitechecker/management/commands/check_sites.py
+++ sitechecker/management/commands/check_sites.py
@@ -1,6 +1,8 @@
 """
 Perform an HTTP HEAD request and see if the site is up and return 200_OK message.
 """
+from datetime import datetime
+
 from django.core.management.base import BaseCommand, CommandError
 import requests
 
@@ -21,5 +23,10 @@ class Command(BaseCommand):
         for site in get_url_list():
             response = requests.head(site.url)
             # TODO: Follow redirects
+            # TODO: Multi threads
+            # TODO: fix time
             self.stdout.write("Response: %s - %s" % (response.status_code, site.url))
+            site.last_response_code = str(response.status_code)
+            site.last_response_checked = datetime.now()
+            site.save()
             # Update database record to store the last response & timestamp.
