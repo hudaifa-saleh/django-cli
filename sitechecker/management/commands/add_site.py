1fedf13a4e71c9c4d1104702a49023097b23d27d
hudaifa <hudaifa890@gmail.com>
2024-01-21 03:25:51 +0300

    check the user it exists

--- sitechecker/management/commands/add_site.py
+++ sitechecker/management/commands/add_site.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from sitechecker.models import Site
 
 
@@ -8,13 +10,23 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("url", type=str)
         parser.add_argument("description", type=str)
+        parser.add_argument("username", type=str)
 
     def handle(self, *args, **options):
-        new_site = Site(url=options["url"], description=options["description"])
+        try:
+            user = User.objects.get(username=options["username"])
+        except ObjectDoesNotExist as e:
+            raise CommandError("User not found, %s %s " % (options["username"], e))
+        except MultipleObjectsReturned as e:
+            raise CommandError("Multiple users match username, %s %s " % (options["username"], e))
 
+        existing_match = Site.objects.filter(user=user, url=options["url"])
+        if existing_match.exists():
+            raise CommandError("Site already exist for user: %s & %s" % (user.username, options["url"]))
+
+        new_site = Site(user=user, url=options["url"], description=options["description"])
         try:
             new_site.save()
             self.stdout.write(self.style.SUCCESS("Site added %s - %s " % (options["url"], options["description"])))
-
-        except Exception as e:
-            self.stdout.write(self.style.ERROR("Something went wrong, %s."))
+        except Exception:
+            raise CommandError("Something went wrong, %s.")
--- sitechecker/management/commands/check_sites.py
+++ sitechecker/management/commands/check_sites.py
@@ -1,8 +1,6 @@
 """
 Perform an HTTP HEAD request and see if the site is up and return 200_OK message.
 """
-from datetime import datetime
-
 from django.core.management.base import BaseCommand
 import requests
 from django.utils import timezone
@@ -14,17 +12,23 @@ def get_url_list():
     return Site.objects.all()
 
 
+def site_check(site):
+    pass
+
+
 class Command(BaseCommand):
     help = "Check all sites."
 
     def handle(self, *args, **options):
         self.stdout.write("[*] Check all sites...")
 
+        with ThreadPoolExecutor(max_workers=24) as executor:
+            future = executor.submit(pow, 323, 1235)
+            print(future.result())
+
         # perform http request
         for site in get_url_list():
             response = requests.head(site.url, allow_redirects=True)
-            # TODO: Multi threads
-            # TODO: tie a site to a user
             self.stdout.write("Response For -->  %s - %s" % (response.status_code, site.url))
             site.last_response_code = str(response.status_code)
             site.last_response_checked = timezone.now()
@@ -32,10 +36,8 @@ class Command(BaseCommand):
                 site.save()
             except Exception as e:
                 self.stdout.write(self.style.ERROR("Error updating check: %s - %s" % (e, site)))
-
             try:
                 new_check_entry = Check(site=site, response_code=str(response.status_code))
                 new_check_entry.save()
             except Exception as e:
                 self.stdout.write(self.style.ERROR("Error Adding check: %s - %s" % (e, new_check_entry)))
-            # Update database record to store the last response & timestamp.
